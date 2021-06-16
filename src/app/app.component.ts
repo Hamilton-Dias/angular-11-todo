8d8de529b2ce1cd030190d253506d0f85a288d57
Hamilton Dias <hamiltondias1997@hotmail.com>
2021-06-15 23:34:14 -0300

    listing static todos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,12 @@
 import { Component } from '@angular/core';
 
+interface Tasks {
+  title: string;
+  description: string;
+  limitDate: string;
+  tags: string[];
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +14,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'todo';
+
+  tasks = [
+    {
+      title: 'Tomar banho',
+      description: 'É necessário tomar banho sempre para ficar limpo',
+      limitDate: '20/12/2021',
+      tags: ["Limpeza", "Corpo", "Bem estar"]
+    },
+    {
+      title: 'Comer',
+      description: 'Comer para aguentar o dia inteiro',
+      limitDate: '10/02/2021',
+      tags: ["Corpo", "Bem estar", "Comida"]
+    },
+    {
+      title: 'Trabalhar',
+      description: 'Ir ao trabalho todo dia',
+      limitDate: '05/01/2021',
+      tags: ["Trabalho", "Obrigação"]
+    }
+  ];
 }
