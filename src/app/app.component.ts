28bdb0bd990281220536149a5d4793a32aa0317f
Hamilton Dias <hamiltondias1997@hotmail.com>
2021-06-19 18:14:52 -0300

    backing up

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -21,7 +21,7 @@ export class AppComponent {
     {
       _id: uuidv4(),
       title: 'Tomar banho',
-      description: 'É necessário tomar banho sempre para ficar limpo',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
       limitDate: '20/12/2021',
       tags: ["Limpeza", "Corpo", "Bem estar"]
     },
@@ -43,13 +43,31 @@ export class AppComponent {
 
   removeTask(id: string) {
     this.tasks = this.tasks.filter(task => {
-      return task._id != id
+      return task._id != id;
     })
   }
 
   removeTagFromTask(tagIndex: number, tagToRemove: string) {
     this.tasks[tagIndex].tags = this.tasks[tagIndex].tags.filter(tag => {
-      return tag != tagToRemove
+      return tag != tagToRemove;
     })
+  }
+
+  /*
+    https://lists.w3.org/Archives/Public/w3c-wai-gl/2017AprJun/0951.html
+    -> width médio de um char é uns 8px, para fonte com 16px
+    preciso pegar o "estilo computado" para pegar o CSS fora do style="...."
+  */
+  isTextBiggerThan2Lines(element: HTMLElement, text: string) {
+    const width = element.offsetWidth;
+    const fontSize = 8;
+
+    const lines = text.length / (width / fontSize);
+
+    return lines > 2;
+  }
+
+  toggleText(button: HTMLElement) {
+    button.innerHTML = 'Esconder'
   }
 }
